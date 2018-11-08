998bd74670cbcad0b3c499c0489c6ed86e611072
Tuna0612 <tunaph06527@fpt.edu.vn>
2018-11-09 01:42:26 +0700

    Update EditPET Thêm DogAdapter,Dog,DogDao Edit ListDogActivity Vân vân and Mây Mây

--- app/src/main/java/com/anhtu/tuna/petmanager/EditPET.java
+++ app/src/main/java/com/anhtu/tuna/petmanager/EditPET.java
@@ -6,16 +6,12 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.anhtu.tuna.petmanager.dao.CatDao;
 import com.anhtu.tuna.petmanager.model.Cat;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class EditPET extends AppCompatActivity {
